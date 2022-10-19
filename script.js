7522276daee24a55d15284711d74db1493ccae23
Paolo Debuque <pdebuque@gmail.com>
2022-10-19 11:10:39 -0500

    small changes

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 console.log('js');
 
+$(document).ready(readyNow);
+// once document is ready, load functionalities
+function readyNow() {
+    console.log('jQuery');
+
+    //function to take inputs and add them to the table
+    $('#submitEmployeeButton').on('click', addEmployee);
+
+    $('table button').on('click', deleteRow);
+}
+
+
+// master database of employees
 const employees = [
     {
         firstName: 'Dwight',
@@ -10,10 +23,10 @@ const employees = [
     }
 ];
 
-let totalSalaries=50000;
+let totalSalaries = 0;
 
 function addEmployee() {
-console.log('in addEmployee')
+    console.log('in addEmployee')
     //take inputs and feed into new object
     const newEmployee = {
         firstName: $('#first_name').val(),
@@ -33,25 +46,53 @@ console.log('in addEmployee')
     $('#title').val('');
     $('#salary').val('');
 
-    totalSalaries+=Number(newEmployee.salary);
+    //update total salary figure
+    totalSalaries += Number(newEmployee.salary)/12;
     $('#totalMonthlySalaries').empty().append(totalSalaries);
 
-//add employee to the page
+    //update color of figure
+    if (totalSalaries > 20000) {
+        $('.total').css('background-color', 'red');
+        $('.total').css('color', 'white');
+    } else {
+        $('.total').css('background-color', 'white');
+        $('.total').css('color', 'black');
+    }
+
+    //add employee to the page
     addRow(newEmployee);
 }
 
 function addRow(employee) {
     console.log('in addRow')
-    $('#employeeTable').append(`<tr>
+    $('#employeeTable').append(`<tr id='employee${employee.id}'>
     <th>${employee.firstName}</th>
     <th>${employee.lastName}</th>
     <th>${employee.id}</th>
     <th>${employee.title}</th>
     <th>$${employee.salary}</th>
-    <th><button>Delete</button></th>
+    <th><button class="deleteButton">Delete</button></th>
     </tr>`)
 }
 
+//function to delete given row when button is clicked. Doesn't work when in the readyNow
+
+function deleteRow() {
+    console.log('in deleteRow');
+    $(this).parent().parent().hide();
+
+    //update total salary figure
+
+
+    //update color of figure
+    if (totalSalaries > 20000) {
+        $('.total').css('background-color', 'red');
+        $('.total').css('color', 'white');
+    } else {
+        $('.total').css('background-color', 'white');
+        $('.total').css('color', 'black');
+    }
+}
 
 function totalMonthlySalaries() {
     //sum up all salary values in employees array
@@ -61,12 +102,10 @@ function totalMonthlySalaries() {
 
     //empty and then fill field with correct number
     $('#totalMonthlySalaries').empty().append(totalMonthlySalaries)
+
+    
+
 }
 
-$(document).ready(readyNow);
 
-function readyNow() {
-    console.log('jQuery');
-    $('#submitEmployee').on('click', addEmployee);
-}
 
